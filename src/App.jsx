23e496fd9d55c52c9ad63ecd681eee711cd0aa04
Andrei <soimua@gmail.com>
2023-12-16 13:52:55 +0100

    new file:   .gitignore 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   src/App.css 	new file:   src/App.jsx 	new file:   src/App.test.js 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/reportWebVitals.js 	new file:   src/setupTests.js 	new file:   src/utilities/hucks.js

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css'
+import { useApi } from './utilities/hucks.js'
+
+function App() {
+ const {data, setCity} = useApi()
+  function getLocation(event) {
+    event.preventDefault()
+    const input = new window.FormData(event.target)
+    let location = input.get('queryCity')
+    if(location === undefined) return
+    setCity(location)
+  }
+  return (
+    <div className='app'>
+      <header>
+        <section className='location'>
+          <h1>{data?.location.name}</h1>
+          <h3>{data?.location.country}</h3>
+        </section>
+        <form className='input-wrapper' onSubmit={getLocation}>
+          <input className='input' name='queryCity' placeholder='Madrid, Paris, London' />
+          <button className='icon' type='submit'> 🔍 </button>
+        </form>
+
+      </header>
+      <aside>
+          <img src={data?.current.condition.icon}  alt={`icon with ${data?.current.condition.icon}`} />
+          <h1>{data?.current.temp_c} ℃</h1>
+      </aside>
+      <footer>
+        <p className='current-time'>Updated at {data?.current.last_updated}</p>
+
+      </footer>
+    </div>
+  )
+}
+
+export default App
+
+ 
